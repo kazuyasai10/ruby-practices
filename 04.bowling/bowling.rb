900bde4f9f98000cd77dc34fb8bb08465eabfd10
kazuyasai10 <kazuya.saito@idealfaro.com>
2020-12-09 15:23:22 +0900

    一旦完成

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,49 +1,87 @@
 score = ARGV[0]
 
-def calculate_score(score)
-  score
-end
+TEN_POINTS = 10
+FIRST_PITCH = 1
+SECOND_PITCH = 2
 
-def chars_score_to_i(score)
-  scores = score.chars
-  shots = []
-  scores.each do |score_string|
-    if score_string == 'X'
-      shots << 10
-      shots << 0
-    else
-      shots << score_string.to_i
-    end
-  end
-  shots
-end
-
-def delimiter_score_for_each_frame(shots)
+def make_frame_array_from_score(score)
   frames = []
-  shots.each_slice(2) do |s|
-    frames << s
+  frame = []
+  scores = score.chars
+  scores.each do |s|
+    frame << if s == 'X'
+               TEN_POINTS
+             else
+               s.to_i
+             end
+    if frames[9] != NIL && frames[9][0] == TEN_POINTS && frames[9][1] == TEN_POINTS
+      frames[9] << frame[0]
+    elsif frames[9] != NIL && frames[9].sum == TEN_POINTS
+      frames[9] << frame[0]
+    elsif frame.length == FIRST_PITCH && frame.first == TEN_POINTS # 1投目 && 10本倒れたときストライクの配列は次の行へ
+      frames << frame # framesにframeを代入
+      frame = [] # 次のフレームを定義
+    elsif frame.length == SECOND_PITCH # frameの配列の長さが２であれば、２投目を投げているので１frame終了.
+      frames << frame # framesにframeを代入
+      frame = [] # 次のフレームを定義
+    end
+    
   end
-  frames
+   frames
 end
 
-STRIKE = 10
-SPARE = 10
 
-def sum_score(frames)
+def calc_score(frames)
   point = 0
-  frames.each do |frame|
-    if frame[0] == STRIKE # strike
-        point += 10
-        point += frame[1]
-      elsif frame.sum == SPARE # spare
-        point += frame[1][0] + 10
+  frames.each_with_index do |frame,index|
+  if frames.last != frame
+    if frames[8] != frame
+      
+
+    if frame.first== TEN_POINTS
+      #break if frames[index+1].nil? #nil対策
+      if frame[0] == 10 && frames[index+1][0] == 10 && frames[index+2][0] == 10 # 3回連続ストライク
+        point += 30
+      elsif frame[0] == 10 && frames[index+1][0] == 10 # 2回連続ストライク
+          point += 20 + frames[index+2][0]
       else
-        point += frame.sum
+      point +=  10 + frames[index+1].first + frames[index+1][1].to_i
       end
+    elsif frame.length == SECOND_PITCH  && frame.sum == TEN_POINTS #スペアの場合
+      point += 10
+      break if frames[index+1].nil? 
+      point += frames[index+1].first
+    else
+      point += frame.sum
+    end
+
+  else
+    if frame.first== TEN_POINTS
+      p "８フレーム目"
+      #break if frames[index+1].nil? #nil対策
+      if frame[0] == 10 && frames[index+1][0] == 10 && frames[index+1][1] == 10 # 3回連続ストライク
+        point += 30
+      elsif frame[0] == 10 && frames[index+1][0] == 10 # 2回連続ストライク
+          point += 20 + frames[index+1][0]
+      else
+      point +=  10 + frames[index+1].first + frames[index+1][1].to_i
+      end
+    elsif frame.length == SECOND_PITCH  && frame.sum == TEN_POINTS #スペアの場合
+      point += 10
+      break if frames[index+1].nil? 
+      point += frames[index+1].first
+    else
+      point += frame.sum
+    end
+    
+  end
+
+  else
+    point += frame.sum
+  end
   end
   point
 end
 
-p shots =  chars_score_to_i(score)
-p frames = delimiter_score_for_each_frame(shots)
-p sum_score(frames)
+frames = make_frame_array_from_score(score)
+p calc_score(frames)
--- 04.bowling/bowling_test.rb
+++ 04.bowling/bowling_test.rb
@@ -2,30 +2,40 @@ require 'minitest/autorun'
 require './bowling'
 
 class BowlingScoreTest < Minitest::Test
-  def test_calculate_score
-    assert_equal 1, calculate_score(1)
+  def test_print_score_case1
+    assert_equal [[0, 10], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0], [10], [6, 4, 5]], make_frame_array_from_score('0X90038273X9180X645')
   end
 
-  def test_chars_score_to_integer
-    assert_equal [1, 2], chars_score_to_i('12')
+  def test_print_score_case2
+    assert_equal [[0, 10], [1, 5], [0, 0], [0, 0], [10], [10], [10], [5, 1], [8, 1], [0, 4]], make_frame_array_from_score('0X150000XXX518104')
   end
 
-  def test_delimiter_score
-    assert_equal [[1, 2], [3, 4]], delimiter_score_for_each_frame([1, 2, 3, 4])
+  def test_print_score_case_final_Strike
+    assert_equal [[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0], [10], [10,10,10]], make_frame_array_from_score('6390038273X9180XXXX')
   end
 
-  def test_sum_score
-    assert_equal 10, sum_score([[1, 2], [3, 4]])
+  # def test_calc_score_fainal
+  #   assert_equal 139, calc_score([[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0], [10], [6, 4, 5]])
+  # end
+
+  # def test_calc_score
+  #   assert_equal 107, calc_score([[0, 10], [1, 5], [0, 0], [0, 0], [10], [10], [10], [5, 1], [8, 1], [0, 4]])
+  # end
+
+  def test_calc_score_last
+    assert_equal 164, calc_score([[6, 3], [9, 0], [0, 3], [8, 2], [7, 3], [10], [9, 1], [8, 0], [10], [10,10,10]])
   end
 
-  def test_sum_score_strike
-    assert_equal 18, sum_score([[10, 0], [6, 2]])
-    assert_equal 30, sum_score([[10, 0], [10, 10]])
-  end
+  # def test_calc_score_mini_spare
+  #   assert_equal 25, calc_score([[6, 4], [5, 5]])
+  # end
 
-  def test_sum_score_spare
-    assert_equal 15, sum_score([[6, 4], [5, 0]])
-    assert_equal 20, sum_score([[6, 4], [10, 0]])
-  end
+  # def test_calc_score_mini_strike
+  #   assert_equal 30, calc_score([[10], [10]])
+  # end
+
+  # def test_calc_score_mini
+  #   assert_equal 94, calc_score([[0, 10], [1, 5], [0, 0],[0, 0], [10], [10], [10], [5, 1]])
+  # end
 
 end
