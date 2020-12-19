b2327348dc0d5e8d12361d64277039f532bbeed3
kazuyasai10 <kazuya.saito@idealfaro.com>
2020-12-19 21:59:48 +0900

    繰り返し処理から最終フレーム除外

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -34,17 +34,15 @@ end
 
 def calc_score(total_frames)
   point = total_frames.sum([]).sum # 数字を単純に合計する。次の繰り返しでストライク、スペアのボーナスを加算する
-  total_frames.each_with_index do |frames, index|
-    next unless index != 9
-
+  total_frames.first(9).each_with_index do |frames, index| # 合算のみの最後のフレームは対象外
     if strike?(frames)
       point += total_frames[index + 1][0]
       point += total_frames[index + 1][1] || total_frames[index + 2][0]
+    elsif spare?(frames)
+      point += total_frames[index + 1][0]
     end
-    point += total_frames[index + 1][0] if spare?(frames)
   end
   point
 end
 
 puts main(score_str) if __FILE__ == $PROGRAM_NAME
-
