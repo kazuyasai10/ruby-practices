4b446384f350b6f2ee7e6218e3e62a3bb4bcfe49
kazuyasai10 <kazuya.saito@idealfaro.com>
2020-12-23 11:10:01 +0900

    テストケース修正

--- 05.ls/ls_test.rb
+++ 05.ls/ls_test.rb
@@ -35,7 +35,7 @@ class LsTest < Minitest::Test
       drwxr-xr-x   2 kazuya.saito staff     64 12 22 13:47 directory
       -rw-r--r--   1 kazuya.saito staff      0 12 21 23:10 foo.txt
       -rw-r--r--   1 kazuya.saito staff      0 12 21 23:09 readme
-      -rw-r--r--   1 kazuya.saito staff   4951 12 23 10:51 test.txt
+      -rw-r--r--   1 kazuya.saito staff   4951 12 23 11:02 test.txt
     TEXT
     assert_equal expected, main('./ls_test', { 'a' => false, 'l' => true, 'r' => false })
   end
@@ -43,7 +43,7 @@ class LsTest < Minitest::Test
   def test_ls_lar
     expected = <<~TEXT
       total  16
-      -rw-r--r--   1 kazuya.saito staff   4951 12 23 10:51 test.txt
+      -rw-r--r--   1 kazuya.saito staff   4951 12 23 11:02 test.txt
       -rw-r--r--   1 kazuya.saito staff      0 12 21 23:09 readme
       -rw-r--r--   1 kazuya.saito staff      0 12 21 23:10 foo.txt
       drwxr-xr-x   2 kazuya.saito staff     64 12 22 13:47 directory
