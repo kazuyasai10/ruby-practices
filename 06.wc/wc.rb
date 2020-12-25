53dbe50e479113451e0f8a789e7cdfb6326fdd13
kazuyasai10 <kazuya.saito@idealfaro.com>
2020-12-26 02:05:29 +0900

    提出物

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,51 +1,56 @@
 # frozen_string_literal: true
 
 require 'optparse'
-
+path = ARGV
 options = ARGV.getopts('l')
 
-def main(options = '')
-  items = ruby_wc(options)
+def main(path, options = '')
+  items = ruby_wc(path, options)
   generate_view_format_wc(items)
 end
 
-def ruby_wc(options = '')
+def ruby_wc(path, options = '')
   total_files = []
   files = []
   str = ''
   total_line = 0
   total_word_count = 0
   total_byte_size = 0
-  ARGF.each_line do |line|
+  file_argf = ARGF.class.new(*path)
+  file_argf.each_line do |line|
     str += line
-    next unless ARGF.eof?
+    next unless file_argf.eof?
 
-    files << str.lines.count.to_s.rjust(6)
+    make_array_files(files, total_files, str, file_argf, options)
     total_line += str.lines.count
-    if options['l']
-      files << ARGF.filename if ARGF.filename != '-'
-      total_files << files
-      files = []
-      str = ''
-    else
-      ary = str.split(/\s+/)
-      files << ary.size.to_s.rjust(6)
-      total_word_count += ary.size
-      files << str.bytesize.to_s.rjust(6)
+    if options['l'] == false
+      total_word_count += str.split(/\s+/).size
       total_byte_size += str.bytesize
-      files << ARGF.filename if ARGF.filename != '-'
-      total_files << files
-      files = []
     end
+    files = []
     str = ''
   end
+  total_files(total_files, total_line, total_word_count, total_byte_size, options)
+end
+
+def total_files(total_files, total_line, total_word_count, total_byte_size, options)
   total_files_data = if options['l']
-                       [total_line.to_s.rjust(6), 'total']
+                       [total_line, 'total']
                      else
-                       [total_line.to_s.rjust(6), total_word_count.to_s.rjust(6), total_byte_size.to_s.rjust(6), 'total']
+                       [total_line, total_word_count.to_s.rjust(6), total_byte_size.to_s.rjust(6), 'total']
                      end
   total_files << total_files_data if total_files.size > 1
   total_files
+end
+
+def make_array_files(files, total_files, str, file_argf, options)
+  files << str.lines.count
+  if options['l'] == false
+    files << str.split(/\s+/).size.to_s.rjust(6)
+    files << str.bytesize.to_s.rjust(6)
+  end
+  files << file_argf.filename if file_argf.filename != '-'
+  total_files << files
 end
 
 def generate_view_format_wc(items)
@@ -57,4 +62,5 @@ def generate_view_format_wc(items)
   formatted_item
 end
 
-print main(options) if __FILE__ == $PROGRAM_NAME
+print main(path, options) if __FILE__ == $PROGRAM_NAME
+
--- 06.wc/wc_test.rb
+++ 06.wc/wc_test.rb
@@ -3,12 +3,30 @@
 require 'minitest/autorun'
 require './wc'
 
-# class WcTest < Minitest::Test
-#   def test_wc()
-#     assert_equal '通常のwcですよ。', run(args = ["text.txt"]) 
-#   end
+class WcTest < Minitest::Test
+  def test_wc
+    expected = <<~TEXT
+      1      1     46 ./test_wc/aaa.readme
+      3      3     25 ./test_wc/text.txt
+      4      4     71 total
+    TEXT
+    assert_equal expected, main(['./test_wc/aaa.readme', './test_wc/text.txt'], { 'a' => false, 'l' => false, 'r' => false })
+  end
 
-#   # def test_wc_l
-#   #   assert_equal 'オプションlですよ', main('./wc_test', { 'a' => false, 'l' => true, 'r' => false })
-#   # end
-# end
+  def test_wc_l
+    expected = <<~TEXT
+      1 ./test_wc/aaa.readme
+      3 ./test_wc/text.txt
+      4 total
+    TEXT
+    assert_equal expected, main(['./test_wc/aaa.readme', './test_wc/text.txt'], { 'a' => false, 'l' => true, 'r' => false })
+  end
+
+  def test_wc_1file
+    expected = <<~TEXT
+      1      1     46 ./test_wc/aaa.readme
+    TEXT
+    assert_equal expected, main(['./test_wc/aaa.readme'], { 'a' => false, 'l' => false, 'r' => false })
+  end
+end
+
